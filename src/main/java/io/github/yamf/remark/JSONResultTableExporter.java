17dfd0c508051f19492b9e6269eb07f61ea7965b
Jens Dietrich <jens.dietrich@gmail.com>
2025-10-30 11:03:19 +1300

    added JSON exporter

--- src/main/java/io/github/yamf/remark/JSONResultTableExporter.java
+++ src/main/java/io/github/yamf/remark/JSONResultTableExporter.java
@@ -1,5 +1,8 @@
 package io.github.yamf.remark;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,41 +11,37 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
-import static java.util.stream.Stream.*;
-
-import static com.google.common.collect.Streams.concat;
 
 
+public class JSONResultTableExporter implements ResultTableExporter {
 
-public class TSVResultTableExporter implements ResultTableExporter {
-
-    final static DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.0000");
-    final static Logger LOG = LoggerFactory.getLogger(Main.class);
+    final static Logger LOG = LoggerFactory.getLogger(JSONResultTableExporter.class);
 
     @Override
     public void export(ResultTable table, Path file) throws IOException {
-        List<String> lines = exportToLines(table);
-        Files.write(file,lines);
+        JSONObject json = new JSONObject();
+        table.rows().forEach(row -> {
+            JSONArray array = new JSONArray();
+            row.cells().forEach(cell -> {
+                JSONObject cellJson = new JSONObject();
+                cellJson.put("question",cell.question());
+                cellJson.put("value",cell.value());
+                cellJson.put("possible-answers",toString(cell.allAnswers()));
+                cellJson.put("correct-answers",toString(cell.correctAnswers()));
+                cellJson.put("correctly-selected",toString(cell.correctAnswersSelected()));
+                cellJson.put("incorrectly-selected",toString(cell.incorrectAnswersSelected()));
+                array.put(json);
+            });
+            json.put(row.id(),array);
+        });
+        Files.writeString(file,json.toString(4));
         LOG.info("results written to {}", file);
     }
 
-    // testable
-    static List<String> exportToLines(ResultTable table) {
-        List<String> lines = new ArrayList<>();
-        lines.add(
-            table.headers().stream().collect(Collectors.joining("\t"))
-        );
-        lines.addAll(
-            table.rows().stream()
-                .map( row -> {
-                    return row.cells().stream().map(mark -> DECIMAL_FORMAT.format(mark.value())).collect(Collectors.joining("\t",row.id()+'\t',""));
-                })
-                .collect(Collectors.toUnmodifiableList())
-        );
-        return lines;
+    private static String toString(Collection<String> list) {
+        return list.stream().map(Object::toString).collect(Collectors.joining(",","{","}"));
     }
-
-
 }
