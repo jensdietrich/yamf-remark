a353084280c0ebecc85da3057c5e51ffeff5c902
Jens Dietrich <jens.dietrich@gmail.com>
2025-10-29 14:55:37 +1300

    added formula tests

--- src/test/java/io/github/yamf/remark/PartialCreditsAndPartialDeductionsTests.java
+++ src/test/java/io/github/yamf/remark/PartialCreditsAndPartialDeductionsTests.java
@@ -1,9 +1,7 @@
 package io.github.yamf.remark;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PartialCreditsAndPartialDeductionsTests {
